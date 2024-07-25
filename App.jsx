9c9bc46637041a61413c94ab6f860184b4499f95
DESKTOP-ACDD6TV\User <guewoljohannkionghat@gmail.com>
2024-07-25 17:56:03 +0200

    installation de tailwindcss

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import './global.css'
+
+export default App = () => {
+  return (
+    <View className=" flex-1 items-center justify-center">
+      <Text className="text-red-800">Styling just works! 🎉</Text>
+    </View>
+  );
+};
