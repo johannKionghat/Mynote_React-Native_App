77c8a139d3b79ec8b1b276aedee5722fd9cc766a
DESKTOP-ACDD6TV\User <guewoljohannkionghat@gmail.com>
2024-07-25 18:15:16 +0200

    configuration de redux/reduxtoolkit

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ import './global.css'
 export default App = () => {
   return (
     <View className=" flex-1 items-center justify-center">
-      <Text className="text-red-800">Styling just works! 🎉</Text>
+      <Text className="text-red-800">Mynote</Text>
     </View>
   );
 };
